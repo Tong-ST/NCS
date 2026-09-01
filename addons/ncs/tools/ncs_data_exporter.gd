8e837c38bb9f27f0c3dc12c7111782287d0b823d
Tong-ST <145736642+Tong-ST@users.noreply.github.com>
2026-09-01 18:16:38 +0700

    Improve safety type check for export/import CSV

--- addons/ncs/tools/ncs_data_exporter.gd
+++ addons/ncs/tools/ncs_data_exporter.gd
@@ -15,7 +15,7 @@ func run_export(
 	_scan_directory_recursive(scan_dir, dataset_paths, target_class)
 
 	if dataset_paths.is_empty():
-		push_warning("NCS Exporter: No resources of type '", target_class, "' found in: ", scan_dir)
+		push_warning("Export Ignored: No resources of type '", target_class, "' found in: ", scan_dir)
 		return
 
 	var data_class_map: Dictionary = {}
@@ -95,12 +95,110 @@ func _get_exported_properties(data_block: Resource) -> Array[String]:
 	var exported_props: Array[String] = []
 	var ignore_props = ["script", "resource_name", "resource_local_to_scene", "resource_path"]
 
+	var data_class_name = _get_resource_class_name(data_block)
+	if data_class_name.is_empty():
+		data_class_name = data_block.resource_path.get_file().get_basename()
+
+	var safe_types = [
+		TYPE_INT,
+		TYPE_FLOAT,
+		TYPE_BOOL,
+		TYPE_STRING,
+		TYPE_STRING_NAME,
+		TYPE_VECTOR2,
+		TYPE_VECTOR2I,
+		TYPE_VECTOR3,
+		TYPE_VECTOR3I,
+		TYPE_VECTOR4,
+		TYPE_VECTOR4I,
+		TYPE_RECT2,
+		TYPE_RECT2I,
+		TYPE_COLOR
+	]
+
 	for prop in data_block.get_property_list():
 		var usage = prop["usage"]
 		var name = prop["name"]
+		var type = prop["type"]
+		var hint_string = prop["hint_string"]
+
 		if (usage & PROPERTY_USAGE_EDITOR) and (usage & PROPERTY_USAGE_STORAGE):
 			if not name.begins_with("_") and not name.begins_with("metadata/") and not name in ignore_props:
-				exported_props.append(name)
+
+				if type in safe_types:
+					exported_props.append(name)
+
+				elif type == TYPE_ARRAY:
+					var is_strongly_typed_primitive = false
+					if !hint_string.is_empty():
+						var sub_type = hint_string.get_slice(":", 0).to_int()
+						if sub_type in safe_types:
+							is_strongly_typed_primitive = true
+
+					var current_arr = data_block.get(name)
+					var has_unsafe_element = false
+
+					if current_arr is Array:
+						for item in current_arr:
+							var item_type = typeof(item)
+							if not (item_type in safe_types):
+								has_unsafe_element = true
+								break
+
+					if has_unsafe_element:
+						push_warning(
+								"Export Ignored: [%s -> %s] Arrays cannot hold nested objects or textures."
+								% [data_class_name, name]
+						)
+						continue
+
+					if current_arr.is_empty() and not is_strongly_typed_primitive:
+						push_warning(
+								"Export Ignored: [%s -> %s] Untyped/Unsupport arrays, Support type e.g. Array[int]"
+								% [data_class_name, name]
+						)
+						continue
+
+					exported_props.append(name)
+
+				elif type == TYPE_DICTIONARY:
+					if !hint_string.is_empty() and ";" in hint_string:
+						var dict_metadata = hint_string.get_slice(";", 0)
+						if ":" in dict_metadata:
+							var key_id = dict_metadata.get_slice(":", 0).to_int()
+							var val_id = dict_metadata.get_slice(":", 1).to_int()
+
+							if not (key_id in safe_types) or not (val_id in safe_types):
+								push_warning(
+										"Export Ignored: [%s -> %s] Dictionary type parameters ignored e.g. holds Objects."
+										% [data_class_name, name]
+								)
+								continue
+
+					var current_dict = data_block.get(name)
+					if current_dict is Dictionary:
+						var has_unsafe_element = false
+						for key in current_dict:
+							var key_type = typeof(key)
+							var val_type = typeof(current_dict[key])
+
+							if not (key_type in safe_types) or not (val_type in safe_types):
+								has_unsafe_element = true
+								break
+
+						if has_unsafe_element:
+							push_warning(
+									"Export Ignored: [%s -> %s] Dictionaries cannot store nested objects."
+									% [data_class_name, name]
+							)
+							continue
+						exported_props.append(name)
+				else:
+					push_warning(
+							"Export Ignored: [%s -> %s] Spreadsheets do not support type '%s'."
+							% [data_class_name, name, type_string(type)]
+					)
+					continue
 	return exported_props
 
 
@@ -134,7 +232,16 @@ func _write_csv_files(data_class_map: Dictionary, output_dir: String) -> void:
 			var path_str: String = row["dataset_path"]
 			var d_name: String = path_str.get_file().get_basename()
 			var line: PackedStringArray = [d_name]
-			for p in props: line.append(str(row.get(p)))
+			for p in props:
+				var value = row.get(p)
+				var value_str: String = ""
+				if typeof(value) in [TYPE_ARRAY, TYPE_DICTIONARY]:
+					value_str = JSON.stringify(value)
+				elif typeof(value) in [TYPE_VECTOR2, TYPE_VECTOR3, TYPE_VECTOR4, TYPE_COLOR, TYPE_RECT2]:
+					value_str = var_to_str(value)
+				else:
+					value_str = str(value)
+				line.append(value_str)
 			line.append(path_str)
 			file.store_csv_line(line)
 
